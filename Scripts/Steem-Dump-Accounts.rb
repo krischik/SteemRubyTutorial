864824759e0e49683f1207ba8634924e62ecd2a2
Martin Krischik <krischik@users.sourceforge.net>
2019-01-29 11:23:29 +0100

    Write demo scripts for Part 3 #4

--- Scripts/Steem-Dump-Accounts.rb
+++ Scripts/Steem-Dump-Accounts.rb
@@ -9,7 +9,7 @@
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or ENDIFFTNESS FOR A PARTICULAR PURPOSE.  See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
@@ -55,8 +55,7 @@ else
          pp Accounts
       end
    rescue => error
-      puts "Error reading accounts:".red
-      pp error
+      Kernel::abort("Error reading accounts:\n".red + error.to_s)
    end
 end
 
