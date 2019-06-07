885e3367c84b7fcad8e601e73dce3113c4375ad3
Martin Krischik <krischik@users.sourceforge.net>
2019-06-07 11:43:42 +0200

    Enhancement #15 fine tune the next script

--- Scripts/Steem-Print-SSC-Table-All.rb
+++ Scripts/Steem-Print-SSC-Table-All.rb
@@ -26,12 +26,18 @@ require 'pp'
 require 'colorize'
 require 'radiator'
 
+##
+# amount of rows read from database in a single query. If
+# the overall results exceeds this limit then make multiple
+# queries. 1000 seem to be the standard for Steem queries.
+#
+Query_Limit = 1000
+
 begin
    # create instance to the steem condenser API which
    # will give us access to the median history price
 
    Contracts = Radiator::SSC::Contracts.new
-
 rescue => error
    # I am using Kernel::abort so the code snipped
    # including error handler can be copy pasted into other
@@ -42,33 +48,58 @@ end
 
 if ARGV.length == 0 then
    puts "
-Steem-Print-SSC-Table — Print first 10 rows of a steem engine tabple
+Steem-Print-SSC-Table-All — Print rows from a steem engine table.
 
 Usage:
-   Steem-Print-SSC-Contract contract_name table_name
+   Steem-Print-SSC-Table-All contract_name table_name [column_name column_value]
 
+      contract_name  name of contract to which the table belongs
+      table_name     table name to print rows from
+      column_name    column name to filter result by (optional)
+      column_value   value to filter result by (optional)
+
+      If column_name and column_value are left out print all rows.
 "
+elsif not [2,4].include? ARGV.length then
+   puts "You need to pass either 2 or 4 parameter.".red
 else
-   # read arguments from command line
+   # read arguments from command line. There are two
+   # options:
+   #
+   # 2 parameter: print all rows from a database table.
+   # 4 parameter: print rows from database which match
+   #              the criteria.
 
    _contract = ARGV[0]
    _table = ARGV[1]
+   _query = {}
 
-   # the query
+   if ARGV.length == 4 then
+      # the query parameter is a hash table with column
+      # names as key and column values as value.
 
-   _rows = Contracts.find(
-      contract: _contract,
-      table: _table,
-      query: {
-      },
-      limit: 10,
-      offset: 0
-   )
+      _query[ARGV[2]] = ARGV[3]
+   end
 
-   pp _rows
+   _current = 0
+   loop do
+      _rows = Contracts.find(
+         contract: _contract,
+         table: _table,
+         query: _query,
+         limit: Query_Limit,
+         offset: _current
+      )
+   break if _rows.length == 0
+      pp _rows
+
+      _current = _current + _rows.length
+   end
+
+   puts "Found %1$d rows".green % _current
 end
 
 ############################################################ {{{1 ###########
 # vim: set nowrap tabstop=8 shiftwidth=3 softtabstop=3 expandtab :
 # vim: set textwidth=0 filetype=ruby foldmethod=marker nospell :
-# vim: set spell spelllang=en_gb fileencoding=utf-8 :
+# vim: set spell spelllang=en_gb fileencoding=utf-8 ::
