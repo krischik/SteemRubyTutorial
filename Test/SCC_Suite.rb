37d2c2a879ea71d47bdc6b3ae3fcc0e658539251
Martin Krischik <krischik@users.sourceforge.net>
2019-06-17 15:48:11 +0200

    Enhancement #17 use suite instead of startup script.

---
+++ Test/SCC_Suite.rb
@@ -0,0 +1,25 @@
+#!/opt/local/bin/ruby
+############################################################# {{{1 ##########
+#  Copyright © 2019 Martin Krischik «krischik@users.sourceforge.net»
+#############################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see «http://www.gnu.org/licenses/».
+############################################################# }}}1 ##########
+
+require_relative '../Test/Contract_Test.rb'
+require_relative '../Test/Token_Test.rb'
+
+############################################################ {{{1 ###########
+# vim: set nowrap tabstop=8 shiftwidth=3 softtabstop=3 expandtab :
+# vim: set textwidth=0 filetype=ruby foldmethod=marker nospell :
+# vim: set spell spelllang=en_gb fileencoding=utf-8 :
