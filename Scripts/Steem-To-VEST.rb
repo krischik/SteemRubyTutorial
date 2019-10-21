92cdf2603560ffc9221c04b6afc5473eaa9bc857
Martin Krischik <krischik@users.sourceforge.net>
2019-10-21 10:45:46 +0200

    just add vote value to the Steem ⇔ VEST conversion.

--- Scripts/Steem-To-VEST.rb
+++ Scripts/Steem-To-VEST.rb
@@ -29,7 +29,9 @@ require 'radiator'
 # The Amount class is used in most Scripts so it was
 # moved into a separate file.
 
+require_relative 'Radiator/Reward_Fund'
 require_relative 'Radiator/Amount'
+require_relative 'Radiator/Price'
 
 begin
    # create instance to the steem condenser API which
@@ -70,16 +72,50 @@ else
    _total_vesting_fund_steem = Radiator::Type::Amount.new Global_Properties.total_vesting_fund_steem
    _total_vesting_shares     = Radiator::Type::Amount.new Global_Properties.total_vesting_shares
    _conversion_rate          = _total_vesting_fund_steem.to_f / _total_vesting_shares.to_f
+  
+   # read the  median history value and
+   # Calculate the conversion Rate for Vests to steem
+   # backed dollar. We use the Amount class from Part 2 to
+   # convert the string values into amounts.
+
+   _median_history_price = Radiator::Type::Price.get
+   SBD_Median_Price      = _median_history_price.to_f
+
+   # read the reward funds.
+
+   _reward_fund = Radiator::Type::Reward_Fund.get
+
+   # extract variables needed for the vote estimate. This
+   # is done just once here to reduce the amount of string
+   # parsing needed.
+
+   Recent_Claims  = _reward_fund.recent_claims
+   Reward_Balance = _reward_fund.reward_balance.to_f
 
    # iterate over the valued passed in the command line
 
    Values.each do |value|
-
       # convert the value to steem by dividing with the
       # conversion rate and display the value
 
       _vest = value.to_f / _conversion_rate
-      puts "%1$15.3f STEEM = %2$18.6f VEST" % [value, _vest]
+
+      # calculate actual vesting by adding and subtracting
+      # delegation as well at the final vest for vote
+      # estimate
+
+      _final_vest = _vest.to_f * 1e6
+
+      # convert the value to steem by multiplying with the
+      # calculate the vote value for 100% upvotes
+
+      _weight              = 1.0
+      _max_voting_power    = 1.0
+      _max_power           = (_max_voting_power * _weight) / 50.0
+      _max_rshares         = _max_power * _final_vest
+      _max_vote_value      = (_max_rshares / Recent_Claims) * Reward_Balance * SBD_Median_Price
+
+      puts "%1$15.3f STEEM    = %2$18.6f VEST, 100%% Upvote: %3$6.3f SBD" % [value, _vest, _max_vote_value]
    end
 end
 
